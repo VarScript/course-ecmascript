ac04895e6a39a465f2b2246582fb0f2a2c79ba87
DevHars <vargassanchezfabio01@gmail.com>
2022-08-23 17:57:42 -0500

    string padding and trailing commas

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,5 @@
+const string = 'Hello';
+
+// PadStart try fill the string with the given number
+console.log(string.padStart(6, '_')) // When start add character
+console.log(string.padEnd(6, '_')) // When end add character
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const ages= [15, 20, 21, 23, , , , 30];
+
+console.log(ages);
+console.log(ages.length);
