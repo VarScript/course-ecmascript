9af252de7411d00a16c845da853db9a895307080
DevHars <vargassanchezfabio01@gmail.com>
2022-08-22 19:42:11 -0500

    destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// Arrays destructuring
+
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// Object destructuring
+
+let user = {username: 'fabio', age: 15};
+let { username, age } = user;
+console.log(username, user.age);
