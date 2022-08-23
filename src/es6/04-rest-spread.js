dd13e34395eaf1ffc26509f078ad2eb7d7172cd8
DevHars <vargassanchezfabio01@gmail.com>
2022-08-22 20:30:36 -0500

    spread operator-destructuring

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,22 @@ console.log(a, fruits[1]);
 let user = {username: 'fabio', age: 15};
 let { username, age } = user;
 console.log(username, user.age);
+
+
+// spread operator - destructuring
+
+let person = { name: 'Fabio', age: 28}
+let country = 'COL';
+
+let data = { id:1, ...person, country };
+console.log(data);
+
+// Rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3,);
