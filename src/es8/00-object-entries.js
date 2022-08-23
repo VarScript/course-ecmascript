3155404760efe88a0cdd47c24066219ccd15bc96
DevHars <vargassanchezfabio01@gmail.com>
2022-08-23 16:57:46 -0500

    rename file, add concepts object entires and value

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,2 @@
+const countries = {MX: 'Mexico', CO: 'Colombia', CL:'Chile', PE:'Peru'};
+console.log(Object.entries(countries)); // Object.entries transform each one object in an array
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,2 @@
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru'}
+console.log(Object.values(countries)); // Object.values transform all object in one array
