fcca4c017d85ec5e9fbb5dfbf72908997d03d05c
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 08:43:37 -0500

    optional-chaining: ?

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+    vs: {
+        country: 'CO'
+    },
+    fabio: {
+        country: 'PE'
+    }
+}
+
+// console.log(users.vs.country);
+console.log(users?.vars?.country); // validate if is in the data base
