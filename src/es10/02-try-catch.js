53bc98a044f7abd5ba4f05dd841af7e01496113d
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 08:30:24 -0500

    try-catch and fromEntries

---
+++ src/es10/02-try-catch.js
@@ -0,0 +1,12 @@
+// Syntax of try and catch
+try{
+    hello();
+} catch (error) {
+    console.log(error);
+}
+
+try {
+    anotherFn()
+} catch {
+    console.log('This is a error');
+}
---
+++ src/es10/03-fromentries.js
@@ -0,0 +1,3 @@
+const entries = new Map([['name', 'Fabio'], ['age', 34]]);
+console.log(entries)
+console.log(Object.fromEntries(entries))
