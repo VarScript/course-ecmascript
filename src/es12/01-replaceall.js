ac77a0318498858b7aeb67480f130f323a61c34e
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 09:55:36 -0500

    replaceAll

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+// Replace a string for other 
+
+const string = 'JavaScript is a fantastic programing language';
+
+const replacedString = string.replace('JavaScript', 'TypScrpt')
+
+console.log(replacedString);
