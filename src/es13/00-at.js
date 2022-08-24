3a0ea420198d6ccef693fbb2f9f4a6d7b97c1388
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 10:40:06 -0500

    at

---
+++ src/es13/00-at.js
@@ -0,0 +1,9 @@
+
+import 'package.json/core-js/features/array/at';
+
+const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+
+console.log(array[array.length -1]); // logic: return last number
+
+console.log(array.at(-1));
