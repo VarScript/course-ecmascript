bc294e47858fe529ddb9a536b57bb4fce0fb58c2
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 08:52:26 -0500

    big numbers

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 15686526123156485n; // n is management of numbers
+const anotherBigNumber = BigInt(15686526123156485);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,3 @@
+const anotherNumber = null;
+const validate = anotherNumber ?? 5; // if yhe value is null
+console.log(validate); // if haven't value assign 5
